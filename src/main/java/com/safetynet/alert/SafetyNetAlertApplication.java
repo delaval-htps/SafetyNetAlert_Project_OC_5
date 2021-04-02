cb4550c9dfeb4572f1f741902b93860ec5461674
delaval <delaval.htps@gmail.com>
2021-04-02 16:26:33 +0200

    First Commit
    SpringBoot Starter project

---
+++ src/main/java/com/safetynet/alert/SafetyNetAlertApplication.java
@@ -0,0 +1,13 @@
+package com.safetynet.alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyNetAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyNetAlertApplication.class, args);
+	}
+
+}
