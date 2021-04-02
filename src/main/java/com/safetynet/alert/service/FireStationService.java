6e2bc6eb83897b523f480b8261228270116b28e9
delaval <delaval.htps@gmail.com>
2021-04-02 18:45:43 +0200

    creation of Services

---
+++ src/main/java/com/safetynet/alert/service/FireStationService.java
@@ -0,0 +1,25 @@
+package com.safetynet.alert.service;
+
+import com.safetynet.alert.model.FireStation;
+import com.safetynet.alert.repository.FireStationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FireStationService {
+
+  @Autowired
+  private FireStationRepository fireStationRepository;
+
+  public FireStation getFireStationById(Long id) {
+    return fireStationRepository.getOne(id);
+  }
+
+  public Iterable<FireStation> getFireStations() {
+    return fireStationRepository.findAll();
+  }
+
+  public FireStation saveFireStation(FireStation fireStation) {
+    return fireStationRepository.save(fireStation);
+  }
+}
