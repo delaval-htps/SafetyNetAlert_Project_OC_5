48d7b96d7887b966e708b85585eb9a329b1a4b24
delaval <delaval.htps@gmail.com>
2021-04-13 18:51:57 +0200

    add getMedicationByDesignationAndPosology()
    in MedicationRepository and MedicationService
    to implement LoadDatabaseFromJsonImpl.java

--- src/main/java/com/safetynet/alert/repository/MedicationRepository.java
+++ src/main/java/com/safetynet/alert/repository/MedicationRepository.java
@@ -2,7 +2,11 @@ package com.safetynet.alert.repository;
 
 import com.safetynet.alert.model.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MedicationRepository extends JpaRepository<Medication, Long> {
+
+  @Query("select m from Medication as m where m.designation=?1 and posology=?2")
+  Medication getOneByDesignationAndPosology(String designation, String posology);
 
 }
