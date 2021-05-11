75da96c1292fa504087bc0cd2ccf7c67455b29a8
delaval <delaval.htps@gmail.com>
2021-05-11 17:23:11 +0200

    add springboot-starter-validation
    add hibernate validation in model Person.java

--- src/main/java/com/safetynet/alert/model/Person.java
+++ src/main/java/com/safetynet/alert/model/Person.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Range;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,9 +20,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -29,30 +35,65 @@ public class Person {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column
-  private Long id_Person;
+  private Long idPerson;
 
   @Column
+  @NotNull(message = "this firstName must not be null")
+  @Pattern(
+           regexp = "^[A-Z]+[a-z]*",
+           message = "this firstName must contains first capital letter "
+               + "and for the other letter lowercase  "
+  )
   private String firstName;
 
   @Column
+  @NotNull(message = "this lastName must not be null")
+  @Pattern(
+           regexp = "^[A-Z]+[a-z]*",
+           message = "this firstName must contains first capital letter"
+               + " and for the other letter lowercase  "
+  )
   private String lastName;
 
   @Column
+  // @NotNull(message = "this birthDate must not be null")
   private String birthDate;
 
   @Column
+  @NotNull(message = "this address must not be null")
   private String address;
 
   @Column
+  @NotNull(message = "this city must not be null")
+  @Pattern(
+           regexp = "^[A-Z]+[a-zA-Z ]*",
+           message = "this City must contains first capital letter "
+               + "and for the other letter lowercase  "
+  )
   private String city;
 
   @Column
+  @Range(
+         min = 0,
+         max = 99999,
+         message = "this zip must not be beetween 0 and 99999"
+  )
   private int zip;
 
   @Column
+  @NotNull(message = "this number of phone must not be null")
+  @Pattern(
+           regexp = "[0-9]{3}-[0-9]{3}-[0-9]{4}",
+           message = "this phone number must match "
+               + "with this pattern xxx-xxx-xxxx with x for a integer"
+  )
   private String phone;
 
   @Column
+  @NotNull(message = "this email must be not null")
+  @Email(
+         message = "this field need to be a correct Email, example: john.boyd@email.com"
+  )
   private String email;
 
   @OneToOne(cascade = CascadeType.ALL)
@@ -75,12 +116,5 @@ public class Person {
   @JsonIgnore
   private Set<FireStation> fireStations = new HashSet<>();
 
-  @Override
-  public String toString() {
-    return "Person [id_Person=" + id_Person + ", firstName=" + firstName
-        + ", lastName=" + lastName + ", birthDate=" + birthDate + ", address="
-        + address + ", city=" + city + ", zip=" + zip + ", phone=" + phone
-        + ", email=" + email + ", medicalRecord=" + medicalRecord
-        + ", fireStations=" + fireStations + "]";
-  }
+
 }
