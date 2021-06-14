842d6744cb4bc9969387c3bb3d2415fe9e06314b
delaval <delaval.htps@gmail.com>
2021-06-14 10:30:29 +0200

    Update exceptions for MedicalRecord

---
+++ src/main/java/com/safetynet/alert/exceptions/medicalrecord/MedicalRecordAlreadyExistedException.java
@@ -0,0 +1,9 @@
+package com.safetynet.alert.exceptions.medicalrecord;
+
+public class MedicalRecordAlreadyExistedException extends RuntimeException {
+  public MedicalRecordAlreadyExistedException(String message) {
+
+    super(message);
+
+  }
+}
---
+++ src/main/java/com/safetynet/alert/exceptions/medicalrecord/MedicalRecordNotFoundException.java
@@ -0,0 +1,9 @@
+package com.safetynet.alert.exceptions.medicalrecord;
+
+public class MedicalRecordNotFoundException extends RuntimeException {
+  public MedicalRecordNotFoundException(String message) {
+
+    super(message);
+
+  }
+}
