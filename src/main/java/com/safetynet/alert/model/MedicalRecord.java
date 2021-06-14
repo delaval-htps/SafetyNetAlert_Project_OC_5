1710e55ee8ec9974c0be2db977066d4064b2ec35
delaval <delaval.htps@gmail.com>
2021-06-14 10:28:45 +0200

    add MedicalRecordRestController UT and IT
    update MedicalRecordRestController
    update MedicalRecordService
    update MedicalRecordRepository
    update MedicalRecord
    Refactoring code -> Tests Green OK

--- src/main/java/com/safetynet/alert/model/MedicalRecord.java
+++ src/main/java/com/safetynet/alert/model/MedicalRecord.java
@@ -1,9 +1,13 @@
 package com.safetynet.alert.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.util.HashSet;
 import java.util.Set;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,21 +20,31 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.OrderBy;
+import javax.persistence.Table;
 
 @Getter
 @Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "MedicalRecord")
 @Entity
 public class MedicalRecord {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column
-  private Long id_MedicalRecord;
+  private Long idMedicalRecord;
 
   // declaration of relationship 1:1 to have bidirectionnal relation
   // ( we can with a MedicalRecord add a Person)
-  @OneToOne(mappedBy = "medicalRecord", cascade = CascadeType.ALL)
 
+  @OneToOne(mappedBy = "medicalRecord",
+            cascade = {CascadeType.DETACH,
+                       CascadeType.MERGE,
+                       CascadeType.REFRESH,
+                       CascadeType.PERSIST})
+  @JsonManagedReference
   private Person person;
 
 
@@ -39,14 +53,12 @@ public class MedicalRecord {
               cascade = {CascadeType.DETACH,
                          CascadeType.MERGE,
                          CascadeType.PERSIST,
-                         CascadeType.REFRESH}
-  )
+                         CascadeType.REFRESH})
   @JoinTable(
              name = "attribution_medication",
-             joinColumns = @JoinColumn(name = "id_MedicalRecord"),
-             inverseJoinColumns = @JoinColumn(name = "id_Medication")
-  )
-  @OrderBy("id_Medication") // to impose jsonPath to be ordered by id when response
+             joinColumns = @JoinColumn(name = "idMedicalRecord"),
+             inverseJoinColumns = @JoinColumn(name = "idMedication"))
+  @OrderBy("idMedication") // to impose jsonPath to be ordered by id when response
   private Set<Medication> medications = new HashSet<>();
 
 
@@ -55,14 +67,17 @@ public class MedicalRecord {
               cascade = {CascadeType.DETACH,
                          CascadeType.MERGE,
                          CascadeType.PERSIST,
-                         CascadeType.REFRESH}
-  )
+                         CascadeType.REFRESH})
   @JoinTable(
              name = "attribution_allergy",
-             joinColumns = @JoinColumn(name = "id_MedicalRecord"),
-             inverseJoinColumns = @JoinColumn(name = "id_Allergy")
-  )
-  @OrderBy("id_Allergy") // to impose jsonPath to be ordered by id when response
+             joinColumns = @JoinColumn(name = "idMedicalRecord"),
+             inverseJoinColumns = @JoinColumn(name = "idAllergy"))
+  @OrderBy("idAllergy") // to impose jsonPath to be ordered by id when response
   private Set<Allergy> allergies = new HashSet<>();
 
+  public void clearSet(Set<?> hashSet) {
+
+    hashSet.clear();
+
+  }
 }
--- src/main/java/com/safetynet/alert/service/MedicalRecordService.java
+++ src/main/java/com/safetynet/alert/service/MedicalRecordService.java
@@ -2,24 +2,56 @@ package com.safetynet.alert.service;
 
 import com.safetynet.alert.model.MedicalRecord;
 import com.safetynet.alert.repository.MedicalRecordRepository;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.validation.Valid;
 
 @Service
 public class MedicalRecordService {
 
   @Autowired
   private MedicalRecordRepository medicalRecordRepository;
+  //  @Autowired
+  //  private PersonService personService;
 
-  public MedicalRecord getMedicalRecordById(Long id) {
-    return medicalRecordRepository.getOne(id);
+  public Optional<MedicalRecord> getMedicalRecordById(Long id) {
+
+    return medicalRecordRepository.findById(id);
+
   }
 
   public Iterable<MedicalRecord> getMedicalRecords() {
+
     return medicalRecordRepository.findAll();
+
+  }
+
+
+  public Optional<MedicalRecord> getMedicalRecordJoinAllById(long l) {
+
+    return medicalRecordRepository.getOneJoinAllOtherById(l);
+
+  }
+
+  public Optional<MedicalRecord> getMedicalRecordByNames(@Valid String lastName,
+      @Valid String firstName) {
+
+    return medicalRecordRepository.getOneByNames(lastName, firstName);
+
   }
 
   public MedicalRecord saveMedicalRecord(MedicalRecord medicalRecord) {
+
     return medicalRecordRepository.save(medicalRecord);
+
   }
+
+  public void deleteMedicalRecord(MedicalRecord medicalRecord) {
+
+    medicalRecordRepository.delete(medicalRecord);
+
+  }
+
+
 }
